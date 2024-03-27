e1e72ca9826d0aaf5f396ac6e133ad0d13194fe4
ilianrusev <ilianrusev@gmail.com>
2024-03-27 16:52:03 +0200

    register module

---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'mc-register',
+  standalone: true,
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+  imports: [],
+})
+export class RegisterComponent {}
