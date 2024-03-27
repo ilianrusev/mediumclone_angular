d9e7d842c0bb6d63f53f1d2516446d7d9a512647
ilianrusev <ilianrusev@gmail.com>
2024-03-27 17:57:43 +0200

    create reducer and selector

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -5,13 +5,16 @@ import { Store } from '@ngrx/store';
 import { register } from '../../store/actions';
 import { RegisterRequestInterface } from '../../types/registerRequest.interface';
 import { RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthStateInterface } from '../../types/authsState.interface';
+import { selectIsSubmitting } from '../../store/reducers';
 
 @Component({
   selector: 'mc-register',
   standalone: true,
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
 })
 export class RegisterComponent {
   form = this.fb.nonNullable.group({
@@ -19,8 +22,12 @@ export class RegisterComponent {
     email: ['', Validators.required],
     password: ['', Validators.required],
   });
+  isSubmitting$: Observable<boolean> = this.store.select(selectIsSubmitting);
 
-  constructor(private fb: FormBuilder, private store: Store) {}
+  constructor(
+    private fb: FormBuilder,
+    private store: Store<{ auth: AuthStateInterface }>,
+  ) {}
 
   onSubmit() {
     console.log('form', this.form.getRawValue());
