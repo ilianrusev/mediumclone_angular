8836bc7bba7ab7032b225f01f5a5035dffb6f098
ilianrusev <ilianrusev@gmail.com>
2024-03-27 17:03:54 +0200

    register form layout

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,11 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mc-register',
   standalone: true,
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
-  imports: [],
+  imports: [ReactiveFormsModule],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit() {
+    console.log('form', this.form.getRawValue());
+  }
+}
