845e0219ddf7b0f54a98d57cbcf30ffb759ae7d7
Ilian Rusev <ilianrusev@gmail.com>
2024-03-28 12:00:16 +0200

    create topbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopBarComponent } from './shared/components/topBar/topBar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopBarComponent],
 })
 export class AppComponent {}
