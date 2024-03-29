ecde5ec278e066b037d9f92cd337c618e7bd6f7e
Ilian Rusev <ilianrusev@gmail.com>
2024-03-29 14:11:11 +0200

    create pagination component

--- src/app/shared/components/feed/services/feed.service.ts
+++ src/app/shared/components/feed/services/feed.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { GetFeedResponseInterface } from '../types/getFeedResponse.interfa';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
