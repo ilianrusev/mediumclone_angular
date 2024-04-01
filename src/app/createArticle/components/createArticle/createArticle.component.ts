69f3d9765a1a315cc23daa7573681fdba777a1ce
Ilian Rusev <ilianrusev@gmail.com>
2024-04-01 21:31:56 +0300

    create the article store

--- src/app/createArticle/components/createArticle/createArticle.component.ts
+++ src/app/createArticle/components/createArticle/createArticle.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { combineLatest } from 'rxjs';
 import { ArticleForm } from 'src/app/shared/components/articleForm/articleForm.component';
 import { ArticleFormValuesInterface } from 'src/app/shared/components/articleForm/types/articleFormValues';
+import { selectIsSubmitting, selectValidationErrors } from '../../store/reducers';
+import { ArticleRequestInterface } from 'src/app/shared/types/articleRequest.interface';
+import { createArticleActions } from '../../store/actions';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'mc-create-article',
   templateUrl: './createArticle.component.html',
   standalone: true,
-  imports: [ArticleForm],
+  imports: [CommonModule, ArticleForm],
 })
 export class CreateArticleComponent {
   initialValues = {
@@ -16,7 +22,18 @@ export class CreateArticleComponent {
     tagList: [],
   };
 
+  data$ = combineLatest({
+    isSubmitting: this.store.select(selectIsSubmitting),
+    backendErrors: this.store.select(selectValidationErrors),
+  });
+
+  constructor(private store: Store) {}
+
   onSubmit(articleFormValues: ArticleFormValuesInterface): void {
-    console.log(articleFormValues);
+    const request: ArticleRequestInterface = {
+      article: articleFormValues,
+    };
+
+    this.store.dispatch(createArticleActions.createArticle({ request }));
   }
 }
---
+++ src/app/createArticle/store/actions.ts
@@ -0,0 +1,13 @@
+import { createActionGroup, props } from '@ngrx/store';
+import { BackendErrorsInterface } from 'src/app/shared/types/backendErrors.interface';
+import { ArticleRequestInterface } from 'src/app/shared/types/articleRequest.interface';
+import { ArticleInterface } from 'src/app/shared/types/article.interface';
+
+export const createArticleActions = createActionGroup({
+  source: 'create article',
+  events: {
+    'Create article': props<{ request: ArticleRequestInterface }>(),
+    'Create article Success': props<{ article: ArticleInterface }>(),
+    'Create article Failure': props<{ errors: BackendErrorsInterface }>(),
+  },
+});
