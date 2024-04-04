5d4e14ad4799734a31719865bf079233c4ac467b
Ilian Rusev <ilianrusev@gmail.com>
2024-04-04 18:41:21 +0300

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from './shared/components/topBar/topBar.component';
 import { Store } from '@ngrx/store';
 import { authActions } from './auth/store/actions';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterOutlet, TopBarComponent],
+  imports: [RouterOutlet, TopBarComponent, FooterComponent],
 })
 export class AppComponent implements OnInit {
   constructor(private store: Store) {}
